469460354d8be15af665daa781bb5279de28d8bd
alizano93 <alz181093@gmail.com>
2017-01-11 20:37:08 -0600

    Adding getRequest Method example

--- src/main/java/com/cch/biblia/service/impl/BibleServiceImpl.java
+++ src/main/java/com/cch/biblia/service/impl/BibleServiceImpl.java
@@ -1,6 +1,13 @@
 package com.cch.biblia.service.impl;
 
+import java.util.Base64;
 import java.util.List;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import com.cch.biblia.model.Capitulo;
 import com.cch.biblia.model.Libro;
@@ -11,9 +18,16 @@ import com.cch.biblia.service.BibleService;
 
 public class BibleServiceImpl implements BibleService{
 
+	private final String AUTH = "GeGphOrt9ay4flLYOxR3ZwJaHezPFm0BPUai95Nn:X";
+	private final String URL_LIBROS = "URL";
 	@Override
 	public List<Libro> getLibros() {
-		// TODO Auto-generated method stub
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		
+		ResponseEntity<String> response = restTemplate.exchange(URL_LIBROS, HttpMethod.GET, getRequest(), String.class);
+		System.out.println(response.getBody());
 		return null;
 	}
 
@@ -51,6 +65,16 @@ public class BibleServiceImpl implements BibleService{
 	public Pasaje getPasaje(String abrev, int ncap, int verInicial, int verFinal) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	private HttpEntity<String> getRequest(){
+		byte[] plainCredsBytes = AUTH.getBytes();
+		byte[] base64CredsBytes = Base64.getEncoder().encode(plainCredsBytes);
+		String base64Creds = new String(base64CredsBytes);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Basic " + base64Creds);
+		return new HttpEntity<String>(headers);
 	}
 
 }
